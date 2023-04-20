1ba7d0ef18fb0db044eed9d258e5be640bf753a0
Kaiewytt2 <124134559+Kaiewytt2@users.noreply.github.com>
2023-04-20 14:56:29 +0700

    Update func4.cs

--- func4.cs
+++ func4.cs
@@ -1,4 +1,9 @@
-static void Function4()
+namespace ConsoleApp1;
+
+public class func4
+{
+    public static void Function4()
     {
         Console.WriteLine(" Штирлиц ел картошку в мундире. \n Война давно кончилась и он не боялся его испачкать." );
     }
+}
