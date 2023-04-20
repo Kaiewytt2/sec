5a2276b46542547e7a96a35b881e367365c88178
Kaiewytt2 <124134559+Kaiewytt2@users.noreply.github.com>
2023-04-20 14:55:51 +0700

    Update func1.cs

--- func1.cs
+++ func1.cs
@@ -1,5 +1,10 @@
-//первая функция 
-static void Function1()
+namespace ConsoleApp1;
+
+public class func1
+{
+    //первая функция 
+    public static void Function1()
     {
         Console.WriteLine("Да ладно, все норм, я шучу");
     }
+}
