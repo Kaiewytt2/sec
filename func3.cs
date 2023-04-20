c79e1110a02a11f1a1f764797e5f4a3f5f86ef17
Kaiewytt2 <124134559+Kaiewytt2@users.noreply.github.com>
2023-04-20 14:56:17 +0700

    Update func3.cs

--- func3.cs
+++ func3.cs
@@ -1,4 +1,9 @@
- static void Function3()
+namespace ConsoleApp1;
+
+public class func3
+{
+    public static void Function3()
     {
         Console.WriteLine("Пусть будет по вашему, так уж и быть");
     }
+}
